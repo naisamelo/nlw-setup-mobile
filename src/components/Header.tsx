7b51706255d92c4c198f00fc40fdbb2abe376423
naisamelo <naisamelo@gmail.com>
2023-01-23 13:53:59 -0300

    Initial commit project mobile

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+import { Feather } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+
+export function Header() {
+    return (
+        <View className="w-full flex-row items-center justify-between">
+            <Logo />
+        </View>
+    )
+}
+
+//1:54:50
