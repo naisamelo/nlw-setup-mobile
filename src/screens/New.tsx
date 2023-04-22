67814e380b175ed5ab11c8c39ef986c08fa7c050
naisamelo <naisamelo@gmail.com>
2023-04-22 10:58:46 -0300

    Creating button confirm

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from 'react-native';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import colors from 'tailwindcss/colors';
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { Feather } from '@expo/vector-icons';
+import { ColorSchemeStore } from "nativewind/dist/style-sheet/color-scheme";
 
 const availableWeekDays = ['Domingo', 'Segund-feira', 'Terça-feira', 'Quarta-geira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
@@ -20,7 +23,10 @@ export function New(){
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
 
         <BackButton/>
 
@@ -34,6 +40,8 @@ export function New(){
 
         <TextInput 
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, beber água, etc..."
+          placeholderTextColor={colors.zinc[400]}
         />
 
         <Text className='font-semibold mt-4 mb-3 text-white text-base'>
@@ -50,6 +58,22 @@ export function New(){
             />
           ))
         }
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather 
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+
+        </TouchableOpacity>
 
       </ScrollView>
     </View>
